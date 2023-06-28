27e689b325eb27794b13d0c1eba4ad15530857a4
pderer <capospi9903@gmail.com>
2023-06-28 14:53:22 +0900

    Tutorial 수정

--- webchats/main.py
+++ webchats/main.py
@@ -2,6 +2,7 @@ import logging
 import pathlib
 
 import jinja2
+import redis.asyncio as redis
 
 import aiohttp_jinja2
 from aiohttp import web
@@ -9,22 +10,26 @@ from views import index
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
 
+
 async def init_app():
     app = web.Application()
     app['websockets'] = {}
     app.on_shutdown.append(shutdown)
     aiohttp_jinja2.setup(
-        app, loader=jinja2.FileSystemLoader(str(BASE_DIR / 'webchats' / 'templates'))
+        app, loader=jinja2.FileSystemLoader(
+            str(BASE_DIR / 'webchats' / 'templates'))
     )
 
     app.router.add_get('/', index)
 
     return app
 
+
 async def shutdown(app):
     for ws in app['websockets'].values():
         await ws.close()
     app['websockets'].clear()
+
 
 async def get_app():
     import aiohttp_debugtoolbar
@@ -32,11 +37,14 @@ async def get_app():
     aiohttp_debugtoolbar.setup(app)
     return app
 
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
+    connection = redis.from_url('redis://localhost:6379')
     app = init_app()
     web.run_app(app)
+
 
 if __name__ == '__main__':
     main()
