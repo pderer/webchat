0b8faecf7988ca8db5c7467097772fe8abcf4790
pderer <capospi9903@gmail.com>
2023-06-29 13:44:27 +0900

    pickle

--- webchats/views.py
+++ webchats/views.py
@@ -1,5 +1,7 @@
+import pickle
 import logging
 
+import redis.asyncio as redis
 import aiohttp
 import aiohttp_jinja2
 from aiohttp import web
@@ -7,16 +9,18 @@ from faker import Faker
 
 log = logging.getLogger(__name__)
 
+
 def get_random_name():
     fake = Faker()
     return fake.name()
+
 
 async def index(request):
     ws_current = web.WebSocketResponse()
     ws_ready = ws_current.can_prepare(request)
     if not ws_ready.ok:
         return aiohttp_jinja2.render_template('index.html', request, {})
-    
+
     await ws_current.prepare(request)
 
     name = get_random_name()
@@ -24,25 +28,36 @@ async def index(request):
 
     await ws_current.send_json({'action': 'connect', 'name': name})
 
-    for ws in request.app['websockets'].values():
-        await ws.send_json({'action': 'join', 'name': name})
-    request.app['websockets'][name] = ws_current
+    # Pickle
+    pickled_ws_current = pickle.dumps(ws_current)
+    connection = redis.from_url("redis://localhost:6379")
+    keys = await redis.keys('*')
+    for key in keys:
+        temp = await redis.get(key)
+        unpickled_temp = pickle.loads(temp)
+        await unpickled_temp.send_json({'action': 'join', 'name': name})
+    await connection.set(name, pickled_ws_current)
 
     while True:
         msg = await ws_current.receive()
 
         if msg.type == aiohttp.WSMsgType.text:
-            for ws in request.app['websockets'].values():
-                if ws is not ws_current:
-                    await ws.send_json(
-                        {'action': 'sent', 'name': name, 'text': msg.data}
-                    )
+            keys = await redis.keys('*')
+            for key in keys:
+                if key is not name:
+                    temp = await redis.get(key)
+                    unpickled_temp = pickle.loads(temp)
+                    await unpickled_temp.send_json({'action': 'join', 'name': name})
         else:
             break
-    
-    del request.app['websockets'][name]
+
+    # Pickle
+    connection.delete(name)
     log.info('%s disconnected.', name)
-    for ws in request.app['websockets'].values():
-        await ws.send_json({'action': 'disconnect', 'name': name})
-    
+    keys = await redis.keys('*')
+    for key in keys:
+        temp = await redis.get(key)
+        unpickled_temp = pickle.loads(temp)
+        await unpickled_temp.send_json({'action': 'disconnect', 'name': name})
+
     return ws_current
