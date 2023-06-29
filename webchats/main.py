32e8a1053e24d1029b8cfc4a5b28acf25b45f742
pderer <capospi9903@gmail.com>
2023-06-29 13:40:39 +0900

    original

--- webchats/main.py
+++ webchats/main.py
@@ -6,6 +6,7 @@ import redis.asyncio as redis
 
 import aiohttp_jinja2
 from aiohttp import web
+from aiohttp_session import setup, SimpleCookieStorage
 from views import index
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
@@ -14,11 +15,14 @@ BASE_DIR = pathlib.Path(__file__).parent.parent
 async def init_app():
     app = web.Application()
     app['websockets'] = {}
+    # TODO
+    app['redis'] = redis.from_url("redis://localhost:6379")
     app.on_shutdown.append(shutdown)
     aiohttp_jinja2.setup(
         app, loader=jinja2.FileSystemLoader(
             str(BASE_DIR / 'webchats' / 'templates'))
     )
+    setup(app, SimpleCookieStorage())
 
     app.router.add_get('/', index)
 
@@ -26,6 +30,7 @@ async def init_app():
 
 
 async def shutdown(app):
+    # TODO
     for ws in app['websockets'].values():
         await ws.close()
     app['websockets'].clear()
@@ -41,7 +46,6 @@ async def get_app():
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
-    connection = redis.from_url('redis://localhost:6379')
     app = init_app()
     web.run_app(app)
 
--- webchats/views.py
+++ webchats/views.py
@@ -1,22 +1,28 @@
+import asyncio
 import logging
+import pickle
 
 import aiohttp
 import aiohttp_jinja2
+import redis.asyncio as redis
 from aiohttp import web
+from aiohttp_session import get_session
 from faker import Faker
 
 log = logging.getLogger(__name__)
 
+
 def get_random_name():
     fake = Faker()
     return fake.name()
+
 
 async def index(request):
     ws_current = web.WebSocketResponse()
     ws_ready = ws_current.can_prepare(request)
     if not ws_ready.ok:
         return aiohttp_jinja2.render_template('index.html', request, {})
-    
+
     await ws_current.prepare(request)
 
     name = get_random_name()
@@ -24,6 +30,7 @@ async def index(request):
 
     await ws_current.send_json({'action': 'connect', 'name': name})
 
+    # original
     for ws in request.app['websockets'].values():
         await ws.send_json({'action': 'join', 'name': name})
     request.app['websockets'][name] = ws_current
@@ -32,6 +39,7 @@ async def index(request):
         msg = await ws_current.receive()
 
         if msg.type == aiohttp.WSMsgType.text:
+            # TODO
             for ws in request.app['websockets'].values():
                 if ws is not ws_current:
                     await ws.send_json(
@@ -39,10 +47,13 @@ async def index(request):
                     )
         else:
             break
-    
+
+    # TODO
+
+    # original
     del request.app['websockets'][name]
     log.info('%s disconnected.', name)
     for ws in request.app['websockets'].values():
         await ws.send_json({'action': 'disconnect', 'name': name})
-    
+
     return ws_current
